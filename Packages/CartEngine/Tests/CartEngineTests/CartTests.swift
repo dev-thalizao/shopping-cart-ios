d1fd01a2530a7a633108c8e0dfc81c926c1f17ad
Thales Frigo <thalesfrigo@gmail.com>
2023-06-04 14:10:49 -0300

    Test add method

--- Packages/CartEngine/Tests/CartEngineTests/CartTests.swift
+++ Packages/CartEngine/Tests/CartEngineTests/CartTests.swift
@@ -3,11 +3,21 @@ import XCTest
 
 open class Cart {
     public private(set) var items = [Any]()
+    
+    public func add(_ product: String) {
+        items.append(product)
+    }
 }
 
 final class CartTests: XCTestCase {
     
     func test_init_startsWithNoItems() {
         XCTAssertTrue(Cart().items.isEmpty)
+    }
+    
+    func test_add_insertsOneItem() {
+        let cart = Cart()
+        cart.add("pizza")
+        XCTAssertEqual(cart.items.count, 1)
     }
 }
