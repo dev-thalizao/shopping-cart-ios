633816bd739a2a62da20718400c2217219364955
Thales Frigo <thalesfrigo@gmail.com>
2023-06-06 12:54:27 -0300

    Test mapper with valid products

--- Packages/Marketplace/Tests/MarketplaceTests/Products/Product API/ProductsMapperTests.swift
+++ Packages/Marketplace/Tests/MarketplaceTests/Products/Product API/ProductsMapperTests.swift
@@ -10,8 +10,6 @@ import XCTest
 
 final class ProductsMapper {
     
-    private struct Root: Decodable {}
-    
     private init() {}
     
     enum Error: Swift.Error {
@@ -20,14 +18,54 @@ final class ProductsMapper {
     
     static func map(_ data: Data, from response: HTTPURLResponse) throws -> [Product] {
                 
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
         guard
             response.statusCode == 200,
-            let _ = try? JSONDecoder().decode(Root.self, from: data)
+            let root = try? decoder.decode(Root.self, from: data)
         else {
             throw Error.invalidData
         }
         
-        return []
+        return root.products.map { remoteProduct in
+            return Product(
+                name: remoteProduct.name,
+                image: remoteProduct.image,
+                onSale: remoteProduct.onSale,
+                regularPrice: remoteProduct.regularPrice,
+                actualPrice: remoteProduct.actualPrice,
+                discountPercentage: remoteProduct.discountPercentage,
+                installments: remoteProduct.installments,
+                sizes: remoteProduct.sizes.map { remoteSize in
+                    return Product.Size(available: remoteSize.available, size: remoteSize.size, sku: remoteSize.sku)
+                }
+            )
+        }
+    }
+}
+
+extension ProductsMapper {
+
+    private struct Root: Decodable {
+        let products: [RemoteProduct]
+    }
+
+    private struct RemoteProduct: Decodable {
+        let name: String
+        let image: URL
+        let onSale: Bool
+        let regularPrice: String
+        let actualPrice: String
+        let discountPercentage: String
+        let installments: String
+        let sizes: [RemoteProductSize]
+    }
+
+    private struct RemoteProductSize: Decodable {
+        let available: Bool
+        let size: String
+        let sku: String
     }
 }
 
@@ -57,12 +95,27 @@ final class ProductsMapperTests: XCTestCase {
         let response = HTTPURLResponse(statusCode: 200)
         XCTAssertEqual(try ProductsMapper.map(emptyJSON, from: response), [])
     }
+    
+    func test_map_deliversItemsOn200HTTPResponseWithProductsJSON() throws {
+        let productsJSON = makeProductsJSON()
+        let response = HTTPURLResponse(statusCode: 200)
+        
+        let expectedProducts = [
+            makeDressProduct(),
+            makeBraceletProduct()
+        ]
+        
+        XCTAssertEqual(try ProductsMapper.map(productsJSON, from: response), expectedProducts)
+    }
 }
 
 // MARK: - Helpers
 
 private func emptyJSON() -> Data {
-    return "{}".data(using: .utf8)!
+    let json = """
+    { "products": [] }
+    """
+    return Data(json.utf8)
 }
 
 private func anyURL() -> URL {
@@ -73,4 +126,107 @@ private extension HTTPURLResponse {
     convenience init(statusCode: Int) {
         self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
     }
+}
+
+private func makeDressProduct() -> Product {
+    return Product(
+        name: "VESTIDO TRANSPASSE BOW",
+        image: URL(string: "https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912")!,
+        onSale: false,
+        regularPrice: "R$ 199,90",
+        actualPrice: "R$ 199,90",
+        discountPercentage: "",
+        installments: "3x R$ 66,63",
+        sizes: [
+            Product.Size(available: false, size: "PP", sku: "5807_343_0_PP"),
+            Product.Size(available: true, size: "P", sku: "5807_343_0_P"),
+            Product.Size(available: true, size: "M", sku: "5807_343_0_M"),
+            Product.Size(available: true, size: "G", sku: "5807_343_0_G"),
+            Product.Size(available: false, size: "GG", sku: "5807_343_0_GG"),
+        ]
+    )
+}
+
+private func makeBraceletProduct() -> Product {
+    return Product(
+        name: "PULSEIRA STYLISH",
+        image: URL(string: "https://d3l7rqep7l31az.cloudfront.net/images/products/20001913_009_catalog_1.jpg?")!,
+        onSale: false,
+        regularPrice: "R$ 29,90",
+        actualPrice: "R$ 29,90",
+        discountPercentage: "",
+        installments: "1x R$ 29,90",
+        sizes: [
+            Product.Size(available: true, size: "U", sku: "4279_1000018_0_U")
+        ]
+    )
+}
+
+private func makeProductsJSON() -> Data {
+    return """
+    {
+      "products": [
+        {
+          "name": "VESTIDO TRANSPASSE BOW",
+          "style": "20002605",
+          "code_color": "20002605_613",
+          "color_slug": "tapecaria",
+          "color": "TAPEÇARIA",
+          "on_sale": false,
+          "regular_price": "R$ 199,90",
+          "actual_price": "R$ 199,90",
+          "discount_percentage": "",
+          "installments": "3x R$ 66,63",
+          "image": "https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912",
+          "sizes": [
+            {
+              "available": false,
+              "size": "PP",
+              "sku": "5807_343_0_PP"
+            },
+            {
+              "available": true,
+              "size": "P",
+              "sku": "5807_343_0_P"
+            },
+            {
+              "available": true,
+              "size": "M",
+              "sku": "5807_343_0_M"
+            },
+            {
+              "available": true,
+              "size": "G",
+              "sku": "5807_343_0_G"
+            },
+            {
+              "available": false,
+              "size": "GG",
+              "sku": "5807_343_0_GG"
+            }
+          ]
+        },
+        {
+          "name": "PULSEIRA STYLISH",
+          "style": "20001913",
+          "code_color": "20001913_009",
+          "color_slug": "dourado",
+          "color": "dourado",
+          "on_sale": false,
+          "regular_price": "R$ 29,90",
+          "actual_price": "R$ 29,90",
+          "discount_percentage": "",
+          "installments": "1x R$ 29,90",
+          "image": "https://d3l7rqep7l31az.cloudfront.net/images/products/20001913_009_catalog_1.jpg?",
+          "sizes": [
+            {
+              "available": true,
+              "size": "U",
+              "sku": "4279_1000018_0_U"
+            }
+          ]
+        }
+      ]
+    }
+    """.data(using: .utf8)!
 }
