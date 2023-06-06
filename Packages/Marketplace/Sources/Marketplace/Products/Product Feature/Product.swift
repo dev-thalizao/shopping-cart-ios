b93c1ae8762c6ec9c0026ab9611b62d794b37063
Thales Frigo <thalesfrigo@gmail.com>
2023-06-06 12:05:45 -0300

    Create the product domain

---
+++ Packages/Marketplace/Sources/Marketplace/Products/Product Feature/Product.swift
@@ -0,0 +1,28 @@
+//
+//  Product.swift
+//  
+//
+//  Created by Thales Frigo on 06/06/23.
+//
+
+import Foundation
+
+struct Product {
+    let name: String
+    let image: URL
+    let onSale: Bool
+    let regularPrice: String
+    let actualPrice: String
+    let discountPercentage: String
+    let installments: String
+    let sizes: [Product.Size]
+}
+
+extension Product {
+    
+    struct Size {
+        let available: Bool
+        let size: String
+        let sku: String
+    }
+}
