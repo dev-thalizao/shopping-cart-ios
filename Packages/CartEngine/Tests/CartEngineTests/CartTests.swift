9f22091011f4bb3fe493e7433d962ab56e3be34e
Thales Frigo <thalesfrigo@gmail.com>
2023-06-06 11:21:09 -0300

    Create a way to listen cart changes

--- Packages/CartEngine/Tests/CartEngineTests/CartTests.swift
+++ Packages/CartEngine/Tests/CartEngineTests/CartTests.swift
@@ -1,10 +1,10 @@
 import XCTest
 @testable import CartEngine
 
+public typealias OnCartChange<T: CartProduct> = (Cart<T>) -> Void
+
 open class Cart<T: CartProduct> {
 
-    private(set) var items = [CartItem<T>]()
-    
     public var totalPrice: Double {
         return items.map(\.price).reduce(0, +)
     }
@@ -13,15 +13,23 @@ open class Cart<T: CartProduct> {
         return items.map(\.quantity).reduce(0, +)
     }
     
+    public var onCartChange: OnCartChange<T>?
+    
+    public private(set) var items = [CartItem<T>]()
+    
+    public init() {}
+    
     public func add(_ product: T) {
         for (index, item) in items.enumerated() {
             if item.product == product {
                 items[index] = .init(product: product, quantity: item.quantity + 1)
+                onCartChange?(self)
                 return
             }
         }
         
         items.append(.init(product: product, quantity: 1))
+        onCartChange?(self)
     }
     
     public func remove(_ product: T) {
@@ -33,6 +41,7 @@ open class Cart<T: CartProduct> {
                 } else {
                     items.remove(at: index)
                 }
+                onCartChange?(self)
                 return
             }
         }
@@ -43,7 +52,7 @@ public protocol CartProduct: Equatable {
     var price: Double { get }
 }
 
-public struct CartItem<T: CartProduct> {
+public struct CartItem<T: CartProduct>: Equatable {
     public let product: T
     public let quantity: UInt
     
@@ -113,6 +122,26 @@ final class CartTests: XCTestCase {
         XCTAssertEqual(cart.totalItems, 3)
     }
     
+    func test_delegate_notifiesAllChangesInItems() {
+        let cart = makeSUT()
+        let spy = SpyFoodChanges()
+        cart.onCartChange = spy.cartDidChange(_:)
+        
+        cart.add(.pizza)
+        cart.add(.pizza)
+        cart.add(.meat)
+        cart.remove(.meat)
+        cart.remove(.pizza)
+        
+        XCTAssertEqual(spy.items, [
+            [.init(product: .pizza, quantity: 1)],
+            [.init(product: .pizza, quantity: 2)],
+            [.init(product: .pizza, quantity: 2), .init(product: .meat, quantity: 1)],
+            [.init(product: .pizza, quantity: 2)],
+            [.init(product: .pizza, quantity: 1)],
+        ])
+    }
+    
     // MARK: Helpers
     
     private func makeSUT() -> Cart<Food> {
@@ -120,7 +149,7 @@ final class CartTests: XCTestCase {
     }
 }
 
-private enum Food: CartProduct {
+ private enum Food: CartProduct {
     case pizza
     case meat
     
@@ -129,5 +158,14 @@ private enum Food: CartProduct {
         case .pizza: return 8
         case .meat: return 10
         }
+    }
+}
+
+private class SpyFoodChanges {
+    
+    private(set) var items = [[CartItem<Food>]]()
+    
+    func cartDidChange(_ cart: Cart<Food>) {
+        items.append(cart.items)
     }
 }
