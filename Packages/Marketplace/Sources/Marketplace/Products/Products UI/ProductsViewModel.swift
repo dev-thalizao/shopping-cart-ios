6c18dbc9dff71957cb107e2ecdcb133597eb31b6
Thales Frigo <thalesfrigo@gmail.com>
2023-06-07 16:42:42 -0300

    Create filter logic for products view model

--- Packages/Marketplace/Sources/Marketplace/Products/Products UI/ProductsViewModel.swift
+++ Packages/Marketplace/Sources/Marketplace/Products/Products UI/ProductsViewModel.swift
@@ -11,10 +11,21 @@ public protocol ProductsLoader {
     func load() async throws -> [Product]
 }
 
- public class ProductsViewModel: ObservableObject {
+public class ProductsViewModel: ObservableObject {
     
     @Published private(set) var products = [Product]()
     @Published private(set) var state = State.initial
+    @Published private(set) var filter = Filter.all
+    
+    private var productsCache = [Product]()
+    
+    var canFilter: Bool {
+        if case let .success(products) = state, !products.isEmpty {
+            return true
+        } else {
+            return false
+        }
+    }
     
     private let loader: ProductsLoader
     
@@ -22,15 +33,27 @@ public protocol ProductsLoader {
         self.loader = loader
     }
     
-     @MainActor func loadProducts() async {
-         self.state = .loading
-         
-         do {
-             let products = try await loader.load()
-             self.state = .success(products)
-         } catch {
-             self.state = .failure
-         }
+    @MainActor func loadProducts() async {
+        self.state = .loading
+        
+        do {
+            let products = try await loader.load()
+            self.state = .success(products)
+            self.productsCache = products
+        } catch {
+            self.state = .failure
+            self.productsCache.removeAll()
+        }
+    }
+    
+    func applyFilter(_ filter: Filter) {
+        self.filter = filter
+        switch filter {
+        case .onSale:
+            self.state = .success(productsCache.filter(\.onSale))
+        case .all:
+            self.state = .success(productsCache)
+        }
     }
 }
 
@@ -41,5 +64,10 @@ extension ProductsViewModel {
         case loading
         case success([Product])
         case failure
+    }
+    
+    enum Filter {
+        case all
+        case onSale
     }
 }
--- Packages/Marketplace/Tests/MarketplaceTests/Products/Products UI/ProductsViewModelTests.swift
+++ Packages/Marketplace/Tests/MarketplaceTests/Products/Products UI/ProductsViewModelTests.swift
@@ -15,6 +15,7 @@ final class ProductsViewModelTests: XCTestCase {
         let (sut, _) = makeSUT()
         XCTAssertEqual(sut.products, [])
         XCTAssertEqual(sut.state, .initial)
+        XCTAssertEqual(sut.filter, .all)
     }
     
     func test_loadProducts_returnProductsWithSuccessRequest() async throws {
@@ -41,6 +42,48 @@ final class ProductsViewModelTests: XCTestCase {
 
         XCTAssertEqual(spy.values, [.loading, .failure])
     }
+    
+    func test_canFilter_onlyIsEnabledWithProducts() async throws {
+        let (sut, loader) = makeSUT()
+        loader.completion = { .success([makeDressProduct()]) }
+        
+        XCTAssertEqual(sut.canFilter, false, "pre-condition")
+        
+        await sut.loadProducts()
+
+        XCTAssertEqual(sut.canFilter, true)
+    }
+    
+    func test_applyFilter_returnFilteredProducts() async throws {
+        let (sut, loader) = makeSUT()
+        loader.completion = { .success([makeDressProduct(onSale: true), makeDressProduct(onSale: false)]) }
+
+        let spy = TestObserver(sut.$state)
+        
+        await sut.loadProducts()
+
+        XCTAssertEqual(spy.values, [
+            .loading,
+            .success([makeDressProduct(onSale: true), makeDressProduct(onSale: false)])
+        ])
+        
+        sut.applyFilter(.onSale)
+        
+        XCTAssertEqual(spy.values, [
+            .loading,
+            .success([makeDressProduct(onSale: true), makeDressProduct(onSale: false)]),
+            .success([makeDressProduct(onSale: true)])
+        ])
+            
+        sut.applyFilter(.all)
+            
+        XCTAssertEqual(spy.values, [
+            .loading,
+            .success([makeDressProduct(onSale: true), makeDressProduct(onSale: false)]),
+            .success([makeDressProduct(onSale: true)]),
+            .success([makeDressProduct(onSale: true), makeDressProduct(onSale: false)]),
+        ])
+    }
 }
 
 // MARK: - Helpers
@@ -64,11 +107,11 @@ private class StubProductsLoader: ProductsLoader {
     }
 }
 
-private func makeDressProduct() -> Product {
+private func makeDressProduct(onSale: Bool = false) -> Product {
     return Product(
         name: "VESTIDO TRANSPASSE BOW",
         image: URL(string: "https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912")!,
-        onSale: false,
+        onSale: onSale,
         regularPrice: "R$ 199,90",
         actualPrice: "R$ 199,90",
         discountPercentage: "",
